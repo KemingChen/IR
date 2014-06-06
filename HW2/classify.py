b8b810d3675fb7c661352c70a5b7b0578746c9e9
gary62107 <gary62107@gmail.com.tw>
2014-06-06 21:33:39 +0800

    package four main function to classify class

--- HW2/classify.py
+++ HW2/classify.py
@@ -85,42 +85,42 @@ def classifier_exit():
 	raise
 	sys.exit()
 
-def main():
+class Classifier():
+	def __init__(self):
+		self.config = Configuration(config_file='classify.conf')
+		try:
+			self.config.load_configuration()
+			self.config_data = self.config.get_configuration()
+		except:
+			print "Error loading configuration file."
+			print "Classifier aborting."
+			raise 	
+		
+		# config.display_configuration()
+		print self.config
 
-	# Load configuration from file
-	config = Configuration(config_file='classify.conf')
-	try:
-		config.load_configuration()
-		config_data = config.get_configuration()
-	except:
-		print "Error loading configuration file."
-		print "Classifier aborting."
-		raise 	
-	
-	# config.display_configuration()
-	print config
+		#Preporcessor: tokenizer, stemmer, etc.
+		prep_lower = self.config_data['lower']
+		prep_stem = self.config_data['stem']
+		prep_pos = self.config_data['pos']
+		prep_ngram = self.config_data['ngram'] 
+		self.prep = Preprocessor(pattern='\W+', lower=prep_lower, stem=prep_stem, pos=prep_pos, ngram=prep_ngram)
 
-	#Preporcessor: tokenizer, stemmer, etc.
-	prep_lower = config_data['lower']
-	prep_stem = config_data['stem']
-	prep_pos = config_data['pos']
-	prep_ngram = config_data['ngram'] 
-	prep = Preprocessor(pattern='\W+', lower=prep_lower, stem=prep_stem, pos=prep_pos, ngram=prep_ngram)
-
-	ev = Evaluation()
-	ml = Rocchio(verbose=VERBOSE, fold='n/a', config=config, ev=ev)
-	training(config, "TRAIN.jdb", ml, prep, first_n_files=50)
-	ml.do_padding()
-	ml.calculate_training_data()
-	# r.display_idx()
-	ml.diagnose()
-	# config.display_configuration()
-	testing(config, "TEST.jdb", ml, ev, prep, first_n_files=10)
-	
-	k = config_data['k']
-	results = ev.calculate(review_spam=True, k=k)
-	print results
-
+		self.ev = Evaluation()
+		self.ml = Rocchio(verbose=VERBOSE, fold='n/a', config=self.config, ev=self.ev)
+	def train(self,train_file="TRAIN.jdb", train_number=50):
+		training(self.config, train_file, self.ml, self.prep, first_n_files=train_number)
+		self.ml.do_padding()
+		self.ml.calculate_training_data()
+		# r.display_idx()
+		self.ml.diagnose()
+	def test(self, test_file="TEST.jdb", test_number=10):
+		testing(self.config, test_file, self.ml, self.ev, self.prep, first_n_files=test_number)
+		k = self.config_data['k']
+		self.results = self.ev.calculate(review_spam=True, k=k)
+	def result(self):
+		return self.results
+		
 if __name__ == '__main__':
 
 	# Profiling mode is not to be used in production,
@@ -134,6 +134,9 @@ if __name__ == '__main__':
 		p_stats.sort_stats('time').print_stats(10)
 	else:
 		try:
-			main()
+			cl = Classifier()
+			cl.train()
+			cl.test(test_file="TEST.jdb", test_number=10)
+			print cl.result()
 		except:
 			classifier_exit()
